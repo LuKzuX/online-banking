a592a8f9fef04b89a0c3542313b9ac7ab5fb9297
LuKz <kabombr@gmail.com>
2024-05-15 18:22:56 -0400

    a

--- client/src/pages/signin.js
+++ client/src/pages/signin.js
@@ -1,7 +1,32 @@
+import { useState } from "react"
+
 export const Signin = () => {
-  return ( 
-    <div className="">
-      login
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await signin(username, password)
+  }
+
+
+  return (
+    <div className=''>
+      <form className=''>
+        <input
+          placeholder='username'
+          onChange={(e) => {
+            setUsername(e.target.value)
+          }}
+        ></input>
+        <input
+          placeholder='password'
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+        ></input>
+        <button onClick={handleSubmit}>signin</button>
+      </form>
     </div>
-   );
+  )
 }
