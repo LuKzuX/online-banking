96bcf922874f9e8d55eb42ad990381b7b78dd00b
LuKz <kabombr@gmail.com>
2024-05-18 12:08:10 -0400

    a

--- client/src/pages/protected.js
+++ client/src/pages/protected.js
@@ -4,7 +4,10 @@ import { useContext } from "react"
 export const Protected = () => {
   const {token, setToken} = useContext(AuthContext)
   return ( 
-    <div className="">{console.log(token)}</div>
+    <div className="">{console.log(token)}
+    {token && <div className="">WELCOME</div>}
+    </div>
+
    );
 }
  
