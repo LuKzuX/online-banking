73b96ffab707aa71c4a18d010103244fb3bc1247
LuKz <kabombr@gmail.com>
2024-07-01 06:20:59 -0400

    a

--- server/db/connection.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose"
-
-export const connection = (url) => {
-  mongoose.set("strictQuery", false)
-  return mongoose.connect(url)
-}
--- server/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-export const router = express.Router()
-import { createUser, loginUser, home } from "../controllers/user.js"
-import {userAuth} from "../middleware/userAuth.js"
-
-router.post(`/signup`, createUser)
-router.post(`/signin`, loginUser)  
-
-router.get("/home", userAuth, home)
