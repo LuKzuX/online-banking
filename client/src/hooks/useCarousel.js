f01f3ac2cf05783b2214260b22c14c08b87ee8e1
LuKz <kabombr@gmail.com>
2024-07-05 07:16:13 -0400

    a

--- client/src/hooks/useCarousel.js
+++ client/src/hooks/useCarousel.js
@@ -6,19 +6,20 @@ import mainImage4 from "../images/main-image.webp";
 
 export const useCarousel = () => {
   const arr = [mainImage, mainImage2, mainImage3, mainImage4];
-  const [currentPosition, setCurrentPosition] = useState(0);
+  const [currentPosition, setCurrentPosition] = useState(1);
+  const textArr = ["Get started", "Know more", "Explore", "Lorem Ipsum"];
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentPosition((prevPosition) => (prevPosition + 1) % arr.length);
-    }, 5000);
+    }, 4000);
     return () => clearInterval(interval);
-  }, []);
+  }, [arr.length]);
 
   return (
-    <div className="flex h-screen relative">
+    <div className="flex h-screen relative ">
       {arr.map((image, index) => (
-        <div key={index}>
+        <div key={index} className="">
           <div
             className={`image-transition absolute right-0 ${
               currentPosition === index ? "w-screen h-1/2" : "w-0 left-0"
@@ -27,12 +28,20 @@ export const useCarousel = () => {
             <img
               src={image}
               alt={index}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover brightness-50"
             ></img>
+            <button
+              className={`transition transform duration-500 absolute left-6 top-[70%] px-6 py-3 text-white bg-transparent border-2 border-white rounded-xl font-bold ${
+                currentPosition === index
+                  ? "translate-x-0"
+                  : "translate-x-[-50px] opacity-0"
+              }`}
+            >
+              {textArr[index]}
+            </button>
           </div>
         </div>
       ))}
-      <p>fyrguhij</p>
     </div>
   );
 };
