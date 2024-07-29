ecf1b5e7e6749322edf96cd9db7b63794c1f54ab
LuKz <kabombr@gmail.com>
2024-07-29 07:20:42 -0400

    front end commence

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -7,18 +7,20 @@ import { useGetUserCards } from "../hooks/useGetUserCards";
 export const Home = () => {
   const { token } = useAuthContext();
   const { user } = useGetCurrentUser();
-  const {cardList} = useGetUserCards()
+  const { cardList } = useGetUserCards();
   return (
     <div className="">
-      <div className="flex flex-col p-6">
+      <div className="flex flex-col p-6 gap-10">
         <div className="flex flex-col items-center border-2 rounded-xl p-3 text-2xl">
           <h1>Your money:</h1>
           <h1 className="font-semibold">${user && user.user.balance}</h1>
         </div>
         <div className="">
-          {cardList && cardList.map((el) => (
-            <p>{el.cardHolder}</p> //demo
-          ))}
+          <h1 className="text-2xl mb-5">Your Cards</h1>
+          {cardList &&
+            cardList.map((el) => (
+              <div className="relative h-[128px] w-[191.2px] bg-neutral-500 rounded-lg"></div>
+            ))}
         </div>
       </div>
     </div>
