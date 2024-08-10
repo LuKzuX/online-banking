ce3576b8540f27e01a6853ef45b7a07130075de0
Lucas <kabombr@gmail.com>
2024-08-10 15:46:55 -0400

    a

--- client/src/pages/cardPages/cardDetails.js
+++ client/src/pages/cardPages/cardDetails.js
@@ -26,15 +26,39 @@ export const CardDetails = () => {
     getCardData();
   }, [token]);
 
+  const date = new Date(cardInfo.expiresIn);
+                const year = date.getFullYear();
+                const month = date.getMonth();
+
   return (
-    <div>
-      <div className="relative h-[148px] w-[211.2px] bg-gradient-to-bl from-blue-600 to-blue-800 rounded-lg shadow-lg">
-        <div className="absolute px-5 top-[10%]  text-white">
-          <p className="mb-3 font-semibold text-lg">Online Bank</p>
-          <img className=" h-[45px]" src={chip1}></img>
-          <p className="font-mono tracking-widest">{cardInfo.cardNumber}</p>
-        </div>
+    <div
+    key={cardInfo._id}
+    className="relative h-[148px] w-[211.2px] bg-gradient-to-bl from-blue-500 to-blue-800 rounded-lg shadow-lg"
+  >
+    <div
+      className="py-2 px-5 top-[10%] text-white flex flex-col items-start"
+    >
+      <div className="w-full flex items-center justify-betwen text-sm">
+        <p>{cardInfo.isCredit ? "Credit Card" : "Debit Card"}</p>
+        <p className="ml-auto">Online Bank</p>
+      </div>
+      <div className="mt-4">
+        <img className=" h-[40px] " src={chip1}></img>
+        <p className="font-mono tracking-widest">
+          {cardInfo.cardNumber}
+        </p>
+      </div>
+      <div className="flex w-full items-center justify-between">
+        <p className="text-sm mt-3 self-start">
+          {cardInfo.cardHolder}
+        </p>
+        <p className=" text-sm mt-3">
+          {month < 10
+            ? `0${month} / ${year}`
+            : month + " / " + year}
+        </p>
       </div>
     </div>
+  </div>
   );
 };
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,13 +5,13 @@ import { useGetCurrentUser } from "../hooks/useGetCurrentUser";
 import { useGetUserCards } from "../hooks/useGetUserCards";
 import chip1 from "../images/chip1.png";
 import { Chart } from "../components/chart";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { user } = useGetCurrentUser();
   const { cardList } = useGetUserCards();
- 
+
   return (
     <div className="bg-neutral-100">
       <div className="flex flex-col p-6 gap-10">
@@ -19,25 +19,51 @@ export const Home = () => {
           <h1>Balance:</h1>
           <h1 className="font-semibold">${user && user.user.balance}</h1>
         </div>
-        <Chart/>
+        <Chart />
         <div className="flex flex-col items-center">
-          <h1 className="text-2xl mb-5 text-center font-semibold">Your Cards:
-          <div className="w-full h-[2px] main-color mt-1"></div>
+          <h1 className="text-2xl mb-5 text-center font-semibold">
+            Your Cards:
+            <div className="w-full h-[2px] main-color mt-1"></div>
           </h1>
           <div className="flex flex-col gap-6 items-center bg-white py-6 w-full rounded-xl border-2">
             {cardList &&
-              cardList.map((el) => (
-                <div
-                  key={el._id}
-                  className="relative h-[148px] w-[211.2px] bg-gradient-to-bl from-blue-600 to-blue-800 rounded-lg shadow-lg"
-                >
-                  <div onClick={() => navigate(`/card/${el._id}`)} className="absolute px-5 top-[10%]  text-white">
-                    <p className="mb-3 font-semibold text-lg">Online Bank</p>
-                    <img className=" h-[45px] " src={chip1}></img>
-                    <p className="font-mono tracking-widest">{el.cardNumber}</p>
+              cardList.map((el) => {
+                const date = new Date(el.expiresIn);
+                const year = date.getFullYear();
+                const month = date.getMonth();
+                return (
+                  <div
+                    key={el._id}
+                    className="relative h-[148px] w-[211.2px] bg-gradient-to-bl from-blue-500 to-blue-800 rounded-lg shadow-lg"
+                  >
+                    <div
+                      onClick={() => navigate(`/card/${el._id}`)}
+                      className="py-2 px-5 top-[10%] text-white flex flex-col items-start"
+                    >
+                      <div className="w-full flex items-center justify-betwen text-sm">
+                        <p>{el.isCredit ? "Credit Card" : "Debit Card"}</p>
+                        <p className="ml-auto">Online Bank</p>
+                      </div>
+                      <div className="mt-4">
+                        <img className=" h-[40px] " src={chip1}></img>
+                        <p className="font-mono tracking-widest">
+                          {el.cardNumber}
+                        </p>
+                      </div>
+                      <div className="flex w-full items-center justify-between">
+                        <p className="text-sm mt-3 self-start">
+                          {el.cardHolder}
+                        </p>
+                        <p className=" text-sm mt-3">
+                          {month < 10
+                            ? `0${month} / ${year}`
+                            : month + " / " + year}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              ))}
+                );
+              })}
           </div>
         </div>
       </div>
