8d4bbd3eff614e8ea3b30128c897b49c78ea3565
LuKz <kabombr@gmail.com>
2024-09-01 21:03:31 -0400

    a

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import { connection } from "../db/connection.js";
 import { router } from "../routes/routes.js";
 import { errorHandlerMiddleware } from "../middleware/errorHandler.js";
 import bodyParser from "body-parser";
-import { createServer } from 'vercel-node-server';
 
 const app = express();
 
@@ -25,4 +24,4 @@ const start = async () => {
 
 start();
 
-export default createServer(app);  // Vercel expects this export
+export default app;  // Export the app for Vercel
